a9807c3dc1efbd3a3e260dbfed6c9748fe51585e
EarthPeople123 <cheungchunlong4@gmail.com>
2016-09-24 18:46:11 +0800

    Spam Bot Release V.3.0

---
+++ SpamBot V.3.0/SpamBot V.3.0/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace SpamBot_V._3._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Inputbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(2, 258);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(250, 50);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(252, 258);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(250, 50);
+            this.Stop.TabIndex = 2;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Inputbox
+            // 
+            this.Inputbox.Location = new System.Drawing.Point(2, 2);
+            this.Inputbox.Multiline = true;
+            this.Inputbox.Name = "Inputbox";
+            this.Inputbox.Size = new System.Drawing.Size(500, 250);
+            this.Inputbox.TabIndex = 3;
+            this.Inputbox.Text = "Type something here to starting spamming...\r\n";
+            this.Inputbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 312);
+            this.Controls.Add(this.Inputbox);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.TextBox Inputbox;
+    }
+}
+
